56fcdb7bfaa502424006629fc5994bb5cdcd67a3
OB1CHAM <ob1chaam@gmail.com>
2023-08-05 20:40:11 +0500

    import gevent

--- server/wsgi.py
+++ server/wsgi.py
@@ -1,7 +1,11 @@
+import gevent.monkey; gevent.monkey.patch_all()
 from app import app, socketio
 from my_libs.config import Config
+from engineio.async_drivers import gevent
 
 if __name__ == '__main__':
+    port = 8000
     config = Config()
     config.create_files()
+    print(f'Server start on localhost:{port}')
     socketio.run(app, host='0.0.0.0', port=8000)
